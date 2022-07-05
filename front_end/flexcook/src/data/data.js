5b1b1a2a27f8aade1a01e4686d36dff19eb06712
AnthonyMeyers <thony.meyers@gmail.com>
2022-07-05 14:43:54 +0200

    added testobject for construction main view

---
+++ front_end/flexcook/src/data/data.js
@@ -0,0 +1,8 @@
+export const testobject = [
+  { id: 1, name: "Apple" },
+  { id: 2, name: "Pear" },
+  { id: 3, name: "Prume" },
+  { id: 4, name: "Banana" },
+  { id: 5, name: "Escargot" },
+  { id: 6, name: "Tomato" },
+];
--- front_end/flexcook/src/modules/main_pages/Fridge.jsx
+++ front_end/flexcook/src/modules/main_pages/Fridge.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { testobject } from "../../data/data";
 
 const Fridge = () => {
-  return <p>This is the future fridge</p>;
+  return (
+    <section className="fridge">
+      <h3 className="fridge__toptitle">Available items</h3>
+      {testobject && testobject.length > 0 && (
+        <ul className="fridge__list">
+          {testobject &&
+            testobject.map(({ id, name }) => (
+              <li key={id} className="fridge__list__item">
+                {name}
+              </li>
+            ))}
+        </ul>
+      )}
+    </section>
+  );
 };
 
 export default Fridge;
