3f3aec3dd596ddc5ca87f8d6f61cf73f660ba3df
AnthonyMeyers <thony.meyers@gmail.com>
2022-07-07 13:22:05 +0200

    testdata removed, now on api

--- front_end/flexcook/src/data/data.js
+++
@@ -1,9 +0,0 @@
-export const testobject = [
-  { id: 1, name: "Apple" },
-  { id: 2, name: "Pear" },
-  { id: 3, name: "Prume" },
-  { id: 4, name: "Banana" },
-  { id: 5, name: "Escargot" },
-  { id: 6, name: "Tomato" },
-  { id: 7, name: "Chocolat" },
-];
--- front_end/flexcook/src/modules/main_pages/Fridge.jsx
+++ front_end/flexcook/src/modules/main_pages/Fridge.jsx
@@ -1,21 +1,24 @@
-import React from "react";
-import { testobject } from "../../data/data";
+import { useState, useEffect } from "react";
 import FridgeItem from "./FridgeItem";
 import { useGetAllIngredientsQuery } from "../../data/ingredientapi";
 
 const Fridge = () => {
-  const { data } = useGetAllIngredientsQuery();
-  console.log(data);
+  const {
+    data: ingredientData,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useGetAllIngredientsQuery();
+
   return (
     <section className="fridge">
       <h3 className="fridge__toptitle">Available items</h3>
-      {testobject && testobject.length > 0 && (
+      {ingredientData && ingredientData.length > 0 && (
         <div className="container">
           <ul className="fridge__list">
-            {testobject &&
-              testobject.map(({ id, name }) => (
-                <FridgeItem key={id} name={name} />
-              ))}
+            {ingredientData.map(({ id, igtName }) => (
+              <FridgeItem key={id} name={igtName} />
+            ))}
           </ul>
         </div>
       )}
